387301f405b8241dae37770b67c0547648ef5709
Yamartim <54243954+Yamartim@users.noreply.github.com>
2021-08-13 20:53:51 -0300

    processamento de comandos e if bonito

--- env_loader.py
+++ env_loader.py
@@ -9,3 +9,7 @@ PREFIX = getenv("PREFIX")
 MARTIM_ID = getenv("MARTIM_ID")
 SONA_ID = getenv("SONA_ID")
 SONA_ID2 = getenv("SONA_ID2")
+
+DESCRIPTION = getenv("DESCRIPTION")
+
+DEV_CHANNEL_ID = int(getenv("DEV_CHANNEL_ID"))
