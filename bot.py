515b559ec901b1e8cfc409dc3ec4816805412487
Yamartim <54243954+Yamartim@users.noreply.github.com>
2021-08-13 20:43:42 -0300

    umas coisa ae

--- bot.py
+++ bot.py
@@ -1,15 +1,33 @@
-from env_loader import TOKEN
+from env_loader import TOKEN, PREFIX, MARTIM_ID, SONA_ID, SONA_ID2, DESCRIPTION
 from discord import Embed, File
 from discord.ext import commands
 from discord.ext.commands import Bot, CommandNotFound
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
-PREFIX = "honk "
-OWNER_IDS = ["143109930134536192", "440569289670656010", "866520555436245032"] # Martim, Sona, Sona celular
+[MARTIM_ID, SONA_ID, SONA_ID2] # Martim, Sona, Sona celular
 
 scheduler = AsyncIOScheduler()
 
 class GAMSo_Bot(Bot):
+
+    
+    def __init__(self):
+
+        self.ready = False
+        self.command_prefix = ["honk "]
+
+        self.description = DESCRIPTION
+        self.owner_ids = set([MARTIM_ID, SONA_ID, SONA_ID2]) # Martim, Sona, Sona celular
+
+        super().__init__(self.command_prefix, description = self.description)
+
+    def run(self, version, v_flavour = '', load_all_cogs = False):
+
+        print("tamo on")
+
+        return super().run(TOKEN, reconnect = True)
+
+
     async def on_ready(self):
         print('{0.user} is honking!'.format(self))
 
--- env_loader.py
+++ env_loader.py
@@ -4,3 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = getenv('TOKEN')
+
+PREFIX = getenv("PREFIX")
+MARTIM_ID = getenv("MARTIM_ID")
+SONA_ID = getenv("SONA_ID")
+SONA_ID2 = getenv("SONA_ID2")
