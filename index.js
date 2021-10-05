aa2848f183c8e8c1034a0c9a47b459684cbb737b
Infinitemarcus <marcus.natrielli@outlook.com>
2021-10-05 16:57:48 -0300

    Modificações no index.js
    Modificações para tornar a execução, passagem de parâmetros e validação de permissões de administrador mais flexíveis, independente do comando a ser usado.
    
    Muitas coisas estão comentadas pois seriam necessárias outras mudanças na lógica do código para tudo funcionar 100%. Logo, recomendo que os autores do projeto analisem com cuidado.

--- index.js
+++ index.js
@@ -34,13 +34,43 @@ client.on("messageCreate", msg => {
     if(!msg.content.startsWith(PREFIX)) return;
 
     const command = msg.content.replace(PREFIX, "").split(" ");
-
-    if(command[0] === "react-role" || command[0] === "rr") {
-        if(!isAdmin(msg.member)) return msg.channel.send("Comando exclusivo da administração!");
-        client.commands.get('react-role').execute(msg, command.slice(1), client);
-    } else if(command[0] === "play" || command[0] === "p") {
-        client.commands.get('play-music').execute(msg, command.slice(1), client, queue);
-    }
+    
+    /*
+        Observação: a linha abaixo só irá funcionar em duas hipóteses:
+        - Se o nome do comando for o mesmo do arquivo importado no começo do index.js
+        - Se, posteriormente, também for adicionado algum campo de aliases/apelidos de comandos
+            -> Se assim for feito, uma possibilidade seria: 
+             const commandObject = client.commands.get(command) || client.commands.find(cmd => (cmd.aliases && cmd.aliases.includes(command));
+    */
+    
+    const commandObject = client.commands.get(command);
+    
+    if(!commandObject) return;
+    
+    if(commandObject.adminOnly && !isAdmin(msg.member)) return msg.channel.send("Comando exclusivo da administração!");
+    
+    /*
+        Pequena gambiarra abaixo, mas que se implementada corretamente funcionaria assim:
+        - É possível passar uma quantidade "indefinida" de parâmetros para as funções 'execute' dos commandos usando um objeto
+        - Como os objetos em JS são meio que por chave-valor, em cada comando você pode verificar se o campo do objeto que você precisa existe e usá-lo
+        - Ex: no play, verifique se existe o campo "queue", e se existir, use
+    */
+      
+    const argObject = { queue };
+    
+    // Daria pra colocar essa linha abaixo em um try catch, mas pela forma que vocês tratam os erros, não sei se seria tão útil
+    commandObject.execute(msg, command.slice(1), client, argObject);
+    
+    /* Comentei isso porque, se usarem esse pull request aqui, isso se torna desnecessário só que ainda não 100% substituido
+       - Por exemplo, os nomes dos arquivos, pelo que eu entendi, não correspondem 100% ao nome dos comandos
+       - Existe o problema com os alias, como comentado anteriormente
+       
+        if(command[0] === "react-role" || command[0] === "rr") {
+            client.commands.get('react-role').execute(msg, command.slice(1), client);
+        } else if(command[0] === "play" || command[0] === "p") {
+            client.commands.get('play-music').execute(msg, command.slice(1), client, queue);
+        }
+    */
 });
 
 client.login(TOKEN);
